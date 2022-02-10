b30e471ed7c830708d0e60d8da8fe88b48eccc07
Mathew Potts <55670682+mathewpotts@users.noreply.github.com>
2022-02-10 20:13:00 +0000

    cleaned up some code : removed global var and added basic error handling.

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ init_vid = "https://www.youtube.com/watch?v=lm6IU6V-dE8" # Let's all go to the l
 bg_color = "#000000" # Black
 bg_opacity = "50"
 
-# Setting the time for Post time as a global variable
-global WHEN 
-WHEN = time(17,0,0) # Default post time
+# Setting the default post time in UTC
+WHEN_t = time(17,0,0) # Default post time
 
 # Setting number of hours between scheduled posts
 dt = 36  
@@ -53,8 +52,8 @@ async def watch(ctx, link):
     # POST request  
     streamkey = os.environ['STREAMKEY']
     if streamkey == 'null':
-      # Notify that there is no streamkey
-      await channel.send("No streamkey found. Please create a new room.")
+        # Notify that there is no streamkey
+        await channel.send("No streamkey found. Please create a new room.")
     url = f"https://w2g.tv/rooms/{streamkey}/sync_update"
     print(url)
     body = json.dumps({
@@ -71,19 +70,19 @@ async def queue(ctx, link):
     # So given a youtube URL I need to extract the videos title, so I can fill the 'title' key.
     # GET request
     if 'youtu' in link: # if youtube link grab title, since by default the link will be the title of the added queued video
-      params = {"format": "json", "url": link}
-      gurl = requests.Request("GET","https://www.youtube.com/oembed",params=params).prepare().url 
-      data = requests.get(gurl).json()
-      title = data['title']
+        params = {"format": "json", "url": link}
+        gurl = requests.Request("GET","https://www.youtube.com/oembed",params=params).prepare().url 
+        data = requests.get(gurl).json()
+        title = data['title']
     else: # tiktok, vimo, etc.
-      title = link
+        title = link
 
     # POST request  
     # This is gonna need to be replaced so that it will grab the key from a previous message...
     streamkey = os.environ['STREAMKEY']
     if streamkey == 'null':
-      # Notify that there is no streamkey
-      await channel.send("No streamkey found. Please create a new room.")    
+        # Notify that there is no streamkey
+        await channel.send("No streamkey found. Please create a new room.")    
     purl = f"https://w2g.tv/rooms/{streamkey}/playlists/current/playlist_items/sync_update"
     print(purl)
     body = json.dumps({
@@ -122,13 +121,29 @@ async def called_once_a_day():  # Fired every day
 async def get_w2g_channel():
     await bot.wait_until_ready() # Make sure your guild cache is ready so the channel can be found via get_channel
     channel = bot.get_guild(GUILD).get_channel(CHANNEL)
-    bot_msgs = await channel.history(limit=50).filter(check_if_bot).flatten()
-    # for loop to find last_scheduled embed
-    bot_scheduled_msgs = [msg for msg in list(bot_msgs) if '17:00:0' in str(msg.created_at) or '05:00:0' in str(msg.created_at)]
-    last_scheduled = bot_scheduled_msgs[0].created_at.replace(second=0,microsecond=0)
-    log.write(f'''Last Scheduled Post: {last_scheduled}''')
+    
+    # Grabbing messages written to channel by bot
+    bot_msgs = list(await channel.history(limit=50).filter(check_if_bot).flatten())
+    
+    # Case handling of when there are no messages in channel
+    if len(bot_msgs) == 0:
+        log.write("No bot messages in this channel. Creating first W2G link",warn=True)
+        channel.send("Creating first Watch2Gether.")
+        await daily_w2g()
+
+    # for loop to find last_scheduled embed    
+    bot_scheduled_msgs = [msg for msg in bot_msgs if '17:00:0' in str(msg.created_at) or '05:00:0' in str(msg.created_at)]
+
+    # Case handling of when there are no scheduled bot messages in channel  
+    if len(bot_scheduled_msgs) != 0:
+        last_scheduled = bot_scheduled_msgs[0].created_at.replace(second=0,microsecond=0)
+        log.write(f'''Last Scheduled Post: {last_scheduled}''')
+    else:
+        last_scheduled = datetime.combine(datetime.utcnow().date(),WHEN_t)
+        log.write(f"No Last Scheduled Post! Setting Scheduled Post time: {last_scheduled}",warn=True)
+      
     last_embed = bot_msgs[0].embeds # last !w2g command, may include user commands that won't have specific time...
-    return channel, list(bot_msgs), last_scheduled, last_embed
+    return channel, bot_msgs, last_scheduled, last_embed
 
 def check_if_bot(msg):
     # Return True if the message is from this bot and if it is an embed link (i.e., embed strings have no characters)
@@ -137,12 +152,6 @@ def check_if_bot(msg):
 async def set_WHEN():
     channel,msgs,last_scheduled,last_embed = await get_w2g_channel()
 
-    # Case where the bot has no messages in channel
-    if len(msgs) == 0:
-        log.write("No bot messages in this channel. Creating first W2G link",warn=True)
-        await daily_w2g()
-        channel,msgs,last_scheduled,last_embed = await get_w2g_channel() 
-    
     # Post every 36 hours and write to log
     WHEN = last_scheduled + timedelta(hours=dt)
     log.write(f'Preliminary Scheduled Post Time: {WHEN}')
